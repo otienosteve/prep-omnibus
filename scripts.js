e5d449a122b83eab54af9dd6f9972edd318c8c15
otienosteve <steveotieno701@gmail.com>
2022-03-16 15:49:23 +0300

    javascript constructors and proptotypes

--- scripts.js
+++ scripts.js
@@ -80,24 +80,90 @@ function <name>(arg1, arg2, arg3,argN){
 ()=>
     =>
 */
-var email= document.getElementById("email").value;
-c(email)
-var uname=document.getElementById("name").value;
-c(uname)
-var textA=document.getElementById("txt").value;
-c(textA)
-nameerr=document.getElementById("nameerr");
 
-document.getElementById("myForm").addEventListener("submit", (evt)=>{
-if(uname.length<8){
-    evt.preventDefault();
-nameerr.textContent="your name should be more than 3 characters"
+
+// nameerr=document.getElementById("leneerr");
+// emailerr=document.getElementById('emailerr');
+// txterr=document.getElementById('txterr');
+// numerr=document.getElementById('numerr');
+
+// document.getElementById("myForm").addEventListener("submit", (evt)=>{
+//     var email= document.getElementById("email").value;
+
+//     var uname=document.getElementById("name").value;
+
+//     var textA=document.getElementById("txt").value;
+//     if(uname.length<4){
+//     c(uname.length)
+//     evt.preventDefault();
+//     nameerr.textContent="your name should be at least 4 characters"
+//     }
+//     if(uname.match(/[0-9]/g)!==null){
+//     evt.preventDefault();
+//     numerr.textContent="your name should Not  contain Numbers"
+//     }
+//     if(!email.includes('@')){
+//         evt.preventDefault();
+//         emailerr.textContent="Enter A valid email"
+//     }
+//     if(textA.split(' ').length<=10){
+//         evt.preventDefault()
+        
+//         txterr.textContent="You should include more than 10 words in your comment";
+//     }
+
+
+
+// })
+// document.getElementById("toggle").addEventListener("click",()=>{
+//     if(document.getElementById("back").style.backgroundColor==="black"){
+//         document.getElementById("back").style.backgroundColor="white"
+//     }else{
+//         document.getElementById("back").style.backgroundColor="black"
+//     }
+// })
+
+
+//literal notation
+//function constructor
+//class 
+
+// let obj1={ name:"omniBus",
+//  purpose:function(){
+//     c(this.name+" we are catching up"); 
+// },
+// time:"was 2 hours",
+// number:65
+// }
+// c(obj1.purpose());
+
+
+// function obj(name, age, designation){
+//     this.name=name;
+//     this.age=age;
+//     this.designation=designation;
+// }
+
+// let person= new obj("steve", "seventy-Five",{des1:"Technical Mentor", des2:"A dad"});
+// c(person.age);
+// obj.prototype.describe= function(){
+//     c(this.age+ " "+this.name+" "+this.designation.des2)
+// }
+// c(person.describe())
+
+class obj{
+
+    constructor(name, age, description){
+        this.age=age;
+        this.name=name;
+        this.description=description;
+    }
+   description(){
+        c(this.age+ " "+this.name+" "+this.description) 
+    }
 }
-if(/^[0-9]*$/.test(uname)){
-    evt.preventDefault();
-nameerr.textContent="your name should Not  contain Numbers"
-}
+
+let person= new obj("Beatrice",60,"Student");
+c(person.description)
 
 
-
-})

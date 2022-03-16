7ce3ff814c55c7ef07e2d47dfaf48cdec234372f
otienosteve <steveotieno701@gmail.com>
2022-03-16 11:18:30 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// let arr=[5,2,9,4,1,6,7,8,3,10];
+// let strArr=['a','b','d','c','f','k','j']
+// //console.log(arr[0]);
+// // let sum=0
+// // for(var i=0; i<=arr.length-1; i++){
+// //     sum+=arr[i];
+// // }
+// // console.log(sum)
+// // let count=0
+// // while(count<=arr.length-1){
+// //     console.log(arr[count]*arr[count]);
+// //     count++;
+// // }
+
+
+// //map()
+// //foreach()
+// //reduce()
+// //filter()
+// //sort()
+// //reverse()
+// //callback function- a function that is passed to other a functions as an argument
+// // let squares=arr.map((elem)=>elem*elem)
+// // let add1=arr.map((elem)=>elem+1)
+// // let addNames=arr.map((elem)=>{
+// //     var name="Number "+ elem.toString();
+// //     return name
+// // });
+// // // console.log(arr);
+// // // console.log(squares);
+// // // console.log(add1);
+// // // console.log(addNames);
+
+// // let doubled=[] 
+// // arr.forEach((elem)=>doubled.push(elem*2));
+
+// // console.log(doubled);
+// //takes an accumulator and a value
+// // let sum2=arr.reduce((ac,val)=>ac+=val*val,2);
+// // console.log(sum2);
+// // let greater5=arr.filter((elem)=> elem<5);
+// // console.log(greater5);
+
+// // let sorted=arr.sort((a,b)=>a-b)
+// // console.log(sorted);
+// let reversed=strArr.reverse()
+// console.log(reversed);
